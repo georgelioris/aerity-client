fe647ee1087fb14c6b7fe3e510df63c442708c1e
George Lioris <george.lioris@gmail.com>
2019-12-22 01:50:05 +0200

    Added loading animation

---
+++ components/spinner.jsx
@@ -0,0 +1,36 @@
+import { Box } from 'grommet';
+
+const spinning = (
+  <svg
+    version="1.1"
+    viewBox="0 0 32 32"
+    width="32px"
+    height="32px"
+    fill="#333333"
+  >
+    <path
+      opacity=".25"
+      d="M16 0 A16 16 0 0 0 16 32 A16 16 0 0 0 16 0 M16 4 A12 12 0 0 1 16 28 
+      A12 12 0 0 1 16 4"
+      fill="white"
+    />
+    <path d="M16 0 A16 16 0 0 1 32 16 L28 16 A12 12 0 0 0 16 4z" fill="white">
+      <animateTransform
+        attributeName="transform"
+        type="rotate"
+        from="0 16 16"
+        to="360 16 16"
+        dur="0.8s"
+        repeatCount="indefinite"
+      />
+    </path>
+  </svg>
+);
+
+const Spinner = () => (
+  <Box align="center" justify="center">
+    {spinning}
+  </Box>
+);
+
+export default Spinner;
