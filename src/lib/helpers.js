d0fcb9efdec8ff22961b66c93cf9537d3e249d11
George Lioris <george.lioris@gmail.com>
2020-06-04 13:30:46 +0300

    More helper methods implemented

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,12 +1,16 @@
 export const dskyKey = process.env.REACT_APP_DARKSKY_API_KEY;
 export const formatUrl = query =>
-  `https://aerity-server.netlify.com/.netlify/functions/server/weather/${query}`;
+  `https://aerity-server.netlify.app/.netlify/functions/server/weather/${query}`;
 export const formatQuery = ({ lat, lon }) => {
   return `${lat.toFixed(4)}-${lon.toFixed(4)}`;
 };
 export const formatIcon = icon => icon.replace(/-/g, '_').toUpperCase();
-export const parseDate = seconds =>
-  new Date(new Date(0).setUTCSeconds(seconds)).toDateString();
+export const parseDate = (seconds, opts) =>
+  opts === 'long'
+    ? new Date(new Date(0).setUTCSeconds(seconds)).toLocaleDateString('en-US', {
+        weekday: 'long'
+      })
+    : new Date(new Date(0).setUTCSeconds(seconds)).toDateString();
 export const parseTemp = (val, units) =>
   `${Math.round(val)}${units === 'si' ? '°C' : '°F'}`;
 export const initState = {
@@ -16,9 +20,16 @@ export const initState = {
   wData: null
 };
 export const isExpired = miliseconds => Date.now() - miliseconds > 300000;
-export const cachedState = (loc, data) =>
+export const formatCache = (loc, data) =>
   JSON.stringify({
     geoLoc: loc,
     data: JSON.stringify(data),
     ts: Date.now()
   });
+export const localCache = name => {
+  const cache = JSON.parse(localStorage.getItem(name)) || null;
+  function setCache(objName, obj) {
+    localStorage.setItem(objName, obj);
+  }
+  return [cache, setCache];
+};
