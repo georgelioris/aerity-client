8e6288602484cb3f7b21c20856f7791d067f1a66
George Lioris <george.lioris@gmail.com>
2020-10-03 06:20:02 +0300

    Added more helpers and moved some to components

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,16 +1,32 @@
 export const formatUrl = ({ lat, lon }) =>
-  `${process.env.REACT_APP_WEATHER_API}/${lat.toFixed(4)}-${lon.toFixed(4)}`;
-export const formatIcon = (icon) => icon.replace(/-/g, '_').toUpperCase();
-export const parseDate = (seconds) =>
-  new Date(new Date(0).setUTCSeconds(seconds)).toDateString();
-export const parseTemp = (val, units) =>
-  `${Math.round(val)}${units === 'si' ? '°C' : '°F'}`;
+  `${lat.toFixed(4)},${lon.toFixed(4)}`;
+export const parseDate = (
+  seconds,
+  options = {
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric'
+  }
+) =>
+  new Intl.DateTimeFormat('en', options).format(
+    new Date(0).setUTCSeconds(seconds)
+  );
+export const displayHighLow = (tmpH, tmpL) => {
+  const [degSymbol, dotSymbol, upArrow, downArrow] = String.fromCharCode(
+    176,
+    0x2022,
+    8593,
+    8595
+  );
+  const [high, low] = [tmpH, tmpL].map((temp) => Math.round(temp) + degSymbol);
+  return `${high}${upArrow} ${dotSymbol} ${low}${downArrow}`;
+};
 export const initState = {
   isLoading: false,
   isError: { status: false, message: '' },
   geoLoc: null,
-  wData: null,
-  button: true
+  wData: null
 };
 export const isExpired = (miliseconds) => Date.now() - miliseconds > 300000;
 export const cachedState = (loc, data) =>
@@ -19,3 +35,19 @@ export const cachedState = (loc, data) =>
     data: JSON.stringify(data),
     ts: Date.now()
   });
+export const isSI = (units) =>
+  units === 'si' || units === 'ca' || units === 'uk2';
+export const toKelvin = (units) => (degrees) =>
+  isSI(units) ? degrees + 273 : ((degrees + 459.67) * 5) / 9;
+export const numberWithCommas = (x) =>
+  x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+export const parseUvIndex = (uvidx) => {
+  const steps = [0, 3, 6, 8, 11];
+  const labels = ['Low', 'Moderate', 'High', 'Very High', 'Extreme'];
+  const scale = steps.reduce(
+    (acc, step, idx) => (uvidx >= step ? idx : acc),
+    4
+  );
+  return `${labels[scale]}, ${uvidx}`;
+};
